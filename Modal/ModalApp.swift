d3888899e76810ae6f327122247ca535acf80576
Eяw!и Gontier <ierwinx@me.com>
2022-08-01 18:42:34 -0500

    Initial Commit

---
+++ Modal/ModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalApp.swift
+//  Modal
+//
+//  Created by Erwin Luz León on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
